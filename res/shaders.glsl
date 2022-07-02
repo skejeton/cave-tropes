317f129ebde047901ce37e1dd549c7e3a3b49881
ishidex2 <ishidex2@gmail.com>
2022-07-02 23:02:19 +0300

    Bare bones lighting

--- res/shaders.glsl
+++ res/shaders.glsl
@@ -19,9 +19,17 @@ void main() {
 in vec2 fs_uv;
 in vec3 fs_normal;
 out vec4 frag_color;
+const float fog_distance = 10.0;
+const float ambient = 0.5;
+const vec3 light = vec3(0.1, 1.0, 0.3);
 
 void main() {
-    frag_color = vec4((fs_uv.y+fs_uv.x+fs_normal)/4.0+vec3(0.5, 0.5, 0.5), 1.0);
+    vec3 color = (fs_uv.y+fs_uv.x+fs_normal)/4.0+vec3(0.5, 0.5, 0.5);
+    float factor = max(dot(fs_normal, normalize(light)), 0.0);
+    factor += 0.7;
+    factor *= smoothstep(0.3, 1, gl_FragCoord.w*fog_distance);
+
+    frag_color = vec4(color * clamp(factor, 0.0, 1.0), 1.0);
 }
 @end
 
